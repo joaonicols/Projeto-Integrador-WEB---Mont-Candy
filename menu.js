5c0a5feee032d3e6af443f2dd3b5cc192e3e905f
hugoblack27 <hugohenrique9341@gmail.com>
2025-09-09 11:52:47 -0300

    arrumando a barra com as três linhas quando fica responsivo

--- menu.js
+++ menu.js
@@ -1,8 +1,4 @@
-function toggleMenu() {
-    const menu = document.getElementById('menu')
-    if (menu.style.display === 'block') {
-        menu.style.display = 'none'
-    } else {
-        menu.style.display = 'block'
-    }
-}
+  function toggleMenu() {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('active');
+  }
